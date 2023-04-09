f7d788754cf507a50056b95a87870f64ada30ca6
Vorobeyyyyyy <mrskilkchat@gmail.com>
2023-04-09 17:01:48 +0300

    Initial commit

---
+++ src/main/kotlin/com/vorobey/kufargod/KufarGodApplication.kt
@@ -0,0 +1,11 @@
+package com.vorobey.kufargod
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KufarGodApplication
+
+fun main(args: Array<String>) {
+    runApplication<KufarGodApplication>(*args)
+}
---
+++ src/test/kotlin/com/vorobey/kufargod/KufarGodApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vorobey.kufargod
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KufarGodApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
